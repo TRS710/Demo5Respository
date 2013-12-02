3d463b4f59b3f21a7efdae3e2c6d05d2b205fed4
FujitsuPOCUser <rakesh.shinde@in.fujitsu.com>
2013-12-02 17:27:27 +0530

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Demo5::Application.config.secret_key_base = 'd12c24e56244fb62a4831f71e2af47cfdb4d87eca4f932488dedf53a734ab58d321d73105c022cf52542a51c5b3ca5a4870f2e068beea1dd0ee104f62d691a24'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Demo5::Application.config.session_store :cookie_store, key: '_Demo5_session'
